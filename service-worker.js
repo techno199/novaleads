5830421050de162adfd3a117ec8864dc2dbc3ea3
ksan199 <techno199@live.ru>
2019-04-06 01:59:40 +0300

    Build update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0cf4320fa3db72e646b313d7f251965d.js"
+  "./precache-manifest.0c83015e3752397ae4fcd2a8822c2283.js"
 );
 
 workbox.clientsClaim();
