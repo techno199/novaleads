60bf1c70a74b446404da3981f1b49f675e7f8452
ksan199 <techno199@live.ru>
2019-04-06 02:03:36 +0300

    Build update

---
+++ precache-manifest.0c83015e3752397ae4fcd2a8822c2283.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "4bf628da83646f27850f",
+    "url": "./static/js/main.2605a119.chunk.js"
+  },
+  {
+    "revision": "e1f98b8ca01a086ef646",
+    "url": "./static/js/2.755d814a.chunk.js"
+  },
+  {
+    "revision": "4bf628da83646f27850f",
+    "url": "./static/css/main.cc7cad7f.chunk.css"
+  },
+  {
+    "revision": "6029e929e4eabcf622f4db85f6d07e60",
+    "url": "./index.html"
+  }
+];
