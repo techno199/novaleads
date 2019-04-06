56d21c5887a15531b31decbc7725658172c7d72b
ksan199 <techno199@live.ru>
2019-04-06 12:05:30 +0300

    Build udate

---
+++ precache-manifest.e3587c7acb6c6c09d2c36084fcb8dad2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "346ae0591e531dbc51d6",
+    "url": "./static/js/main.9288b90d.chunk.js"
+  },
+  {
+    "revision": "6bb0fdd07729de73796a",
+    "url": "./static/js/2.28fead7e.chunk.js"
+  },
+  {
+    "revision": "346ae0591e531dbc51d6",
+    "url": "./static/css/main.cc7cad7f.chunk.css"
+  },
+  {
+    "revision": "602b8736867af345c2ac2ba82ccbcc3e",
+    "url": "./index.html"
+  }
+];
